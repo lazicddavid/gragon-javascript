f9b8955f1ec4811ba27025af1ac3ceb8c9fe9fd6
David Lazic <lazic.ddavid@gmail.com>
2025-05-19 21:23:04 +0200

    added function logStats() {   let stats = Name: , Health: ,   Energy: , Level: , Experience:    , Speed: , Attack Power: ,    Movement: ;   console.log(stats);

--- dragon.js
+++ dragon.js
@@ -1,4 +1,4 @@
-let name = "dragon";
+let monsterName = "dragon";
 let health = 100;
 let energy = 99;
 let level = 12;
@@ -6,3 +6,13 @@ let experience = 30;
 let speed = 250;
 let attackPower = 54;
 let movement = "walking";
+
+console.log("---------------");
+
+function logStats() {
+  let stats = `Name: ${monsterName}, Health: ${health}, 
+  Energy: ${energy}, Level: ${level}, Experience:
+   ${experience}, Speed: ${speed}, Attack Power: ${attackPower},
+   Movement: ${movement}`;
+  console.log(stats);
+}
