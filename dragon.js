32ede39ffd93438096219a4e19717bf7641c0b80
David Lazic <lazic.ddavid@gmail.com>
2025-05-19 21:55:00 +0200

    added function fly

--- dragon.js
+++ dragon.js
@@ -7,12 +7,22 @@ let speed = 250;
 let attackPower = 54;
 let movement = "walking";
 
-console.log("---------------");
-
 function logStats() {
   let stats = `Name: ${monsterName}, Health: ${health}, 
   Energy: ${energy}, Level: ${level}, Experience:
    ${experience}, Speed: ${speed}, Attack Power: ${attackPower},
    Movement: ${movement}`;
   console.log(stats);
+}
+
+function fly() {
+  if (movement === "walking") {
+    movement = "flying";
+    speed += 50;
+    console.log(`${monsterName} is now flying, current speed is ${speed}`);
+  } else if (movement === "flying") {
+    console.log("Monster is already flying.");
+  }
+
+  checkStats();
 }
