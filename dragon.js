fac2c3bc1c751038fc063c3b2745ed23154e4933
David Lazic <lazic.ddavid@gmail.com>
2025-05-25 22:25:50 +0200

    added fly function

--- dragon.js
+++ dragon.js
@@ -165,38 +165,29 @@ toggleStockStatus();
 toggleStockStatus();
 printBookDetails();
 */
-/*
+
 let monster = {
-  name: "dragon"
-  health: 100
-  energy: 99
-  level: 12
-  experience: 30
-  speed: 250
-  attack: 54
-  movement: "walking"
+  name: "dragon",
+  health: 100,
+  energy: 99,
+  level: 12,
+  experience: 30,
+  speed: 250,
+  attack: 54,
+  movement: "walking",
 
   logStats: function () {
     console.log(
       `Name: ${this.name}, Health: ${this.health}, Energy: ${this.energy}, Level: ${this.level}, Experience: ${this.experience}, Speed: ${this.speed}, Movement: ${this.movement}`
     );
-  },*/
+  },
+};
+monster.logStats();
 
-
-
-
-  let = car {
-    brand: "Audi",
-    model: "A4",
-    year: 2020,
-    speed: 0,
+console.log("-------------------- ");
+fly: function () {
+  if (this.movement = "walking") {
+    
+  }
   
-
-    drive: function () {
-      this.speed += 20;
-      console.log(`CAr is now drivig at ${this.speed} km/h`);
-    }
-
-    };
-
-
+}
